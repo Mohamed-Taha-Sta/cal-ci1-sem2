6369a6742b6e0d1ec690e0a9e0925c4466b3bf6e
Taha <mohamedtahasta@gmail.com>
2024-04-27 18:26:30 +0100

    Finishing touches. Really this time.

--- components/Credit.tsx
+++ components/Credit.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Credit = () => {
     return (
         <a href="https://www.linkedin.com/in/mohamed-taha-sta/" target="_blank" rel="noopener noreferrer">
-            <div className={"flex flex-row justify-center"}>
+            <div className={"flex flex-row justify-center pb-6"}>
                 <h2>Made by </h2>
                 <p className={"font-extrabold underline ms-2 text-yellow-600"}> Taha</p>
             </div>
