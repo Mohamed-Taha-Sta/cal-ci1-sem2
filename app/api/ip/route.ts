fcdab9f79bc11ecb4ad0cf6bf8e86a7b27de58dc
Taha <mohamedtahasta@gmail.com>
2024-05-12 17:54:52 +0100

    Fixing a bug where no one has an ip address. Im going insane#2.

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -4,9 +4,5 @@ export const dynamic = 'force-dynamic';
 export function GET(req: NextRequest) {
     // @ts-ignore
     const ip = req.ip
-    return {
-        props: {
-            ip,
-        },
-    }
+    return new Response(ip, {status: 200})
 }
