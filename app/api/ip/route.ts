bd3e648c3f4782294e46f8f3f7c62a14d68224cc
Taha <mohamedtahasta@gmail.com>
2024-05-12 17:51:19 +0100

    Fixing a bug where no one has an ip address. Im going insane.

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -1,14 +1,10 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-// @ts-ignore
-import requestIp from 'request-ip'
-import {NextRequest} from "next/server";
+import type {NextRequest} from "next/server";
 
 export const dynamic = 'force-dynamic';
 
-export function GET(req: any & { url: string }) {
+export function GET(req: NextRequest & { url: string }) {
     // @ts-ignore
-    const forwarded = req.headers["x-forwarded-for"]
-    const ip = forwarded ? forwarded.split(/, /)[0] : req.socket.remoteAddress
+    const ip = req.ip
     return {
         props: {
             ip,
