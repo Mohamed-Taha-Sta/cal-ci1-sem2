0d20e6bec8b22963f56cd3cb4ba30e01e8f81156
Taha <mohamedtahasta@gmail.com>
2024-05-12 17:14:25 +0100

    Debugging for production.

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -2,7 +2,7 @@ import {NextApiRequest, NextApiResponse} from 'next';
 // @ts-ignore
 import requestIp from 'request-ip'
 
-export function GET(req: NextApiRequest) {
+export function GET(req: NextApiRequest & { url: string }) {
     let ip = requestIp.getClientIp(req)
     return new Response(JSON.stringify(ip), {status: 200})
 }
